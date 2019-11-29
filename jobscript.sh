36583ba721b1d1381c33b2fa58be5bf899a1a97b
Jarik van der Heide <jmvdheide96@gmail.com>
2019-11-29 17:07:22 +0100

    First tested jobscript.
    Findings in notes.txt

--- jobscript.sh
+++ jobscript.sh
@@ -6,6 +6,10 @@
 #SBATCH --output=job-%j.log
 #SBATCH --partition=short
 
+module restore
+
+source VirtualEnv/bin/activate
+
 onmt_preprocess -train_src Data/SVO_AN/src-train.txt -train_tgt Data/SVO_AN/tgt-train.txt -valid_src Data/SVO_AN/src-val.txt -valid_tgt Data/SVO_AN/tgt-val.txt -save_data Data/SVO_AN/prepro
 
 # onmt_preprocess -train_src Data/SVO_NA/src-train.txt -train_tgt Data/SVO_NA/tgt-train.txt -valid_src Data/SVO_NA/src-val.txt -valid_tgt Data/SVO_NA/tgt-val.txt -save_data Data/SVO_NA/prepro
@@ -15,12 +19,12 @@ onmt_preprocess -train_src Data/SVO_AN/src-train.txt -train_tgt Data/SVO_AN/tgt-
 # onmt_preprocess -train_src Data/SOV_NA/src-train.txt -train_tgt Data/SOV_NA/tgt-train.txt -valid_src Data/SOV_NA/src-val.txt -valid_tgt Data/SOV_NA/tgt-val.txt -save_data Data/SOV_NA/prepro
 
 
-onmt_train -data Data/SVO_AN -save_model Models/SVO_AN_model
+onmt_train -data Data/SVO_AN/prepro -save_model Models/SVO_AN_model
 
-# onmt_train -data Data/SVO_AN -early_stopping 3 -valid_steps 1000 -save_model Results/SVO_AN_model
+# onmt_train -data Data/SVO_AN/prepro -early_stopping 3 -valid_steps 1000 -save_model Results/SVO_AN_model
 
-# onmt_train -data Data/SVO_NA --early_stopping 3 -valid_steps 1000 -save_model Results/SVO_NA_model
+# onmt_train -data Data/SVO_NA/prepro --early_stopping 3 -valid_steps 1000 -save_model Results/SVO_NA_model
 
-# onmt_train -data Data/SOV_AN --early_stopping 3 -valid_steps 1000 -save_model Results/SOV_AN_model
+# onmt_train -data Data/SOV_AN/prepro --early_stopping 3 -valid_steps 1000 -save_model Results/SOV_AN_model
 
-# onmt_train -data Data/SOV_NA --early_stopping 3 -valid_steps 1000 -save_model Results/SOV_NA_model
+# onmt_train -data Data/SOV_NA/prepro --early_stopping 3 -valid_steps 1000 -save_model Results/SOV_NA_model
