201bb3e87a3c5a101eb43fc7f42dd91990e2002d
Jarik van der Heide <jmvdheide96@gmail.com>
2019-11-29 15:49:07 +0100

    Updated jobscript for tests

--- jobscript.sh
+++ jobscript.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+#SBATCH --job-name=onmt_test
+#SBATCH --time=20:00
+#SBATCH --ntasks=1
+#SBATCH --mem=2000
+#SBATCH --output=job-%j.log
+#SBATCH --partition=short
 
 onmt_preprocess -train_src Data/SVO_AN/src-train.txt -train_tgt Data/SVO_AN/tgt-train.txt -valid_src Data/SVO_AN/src-val.txt -valid_tgt Data/SVO_AN/tgt-val.txt -save_data Data/SVO_AN/prepro
 
-onmt_preprocess -train_src Data/SVO_NA/src-train.txt -train_tgt Data/SVO_NA/tgt-train.txt -valid_src Data/SVO_NA/src-val.txt -valid_tgt Data/SVO_NA/tgt-val.txt -save_data Data/SVO_NA/prepro
+# onmt_preprocess -train_src Data/SVO_NA/src-train.txt -train_tgt Data/SVO_NA/tgt-train.txt -valid_src Data/SVO_NA/src-val.txt -valid_tgt Data/SVO_NA/tgt-val.txt -save_data Data/SVO_NA/prepro
 
-onmt_preprocess -train_src Data/SOV_AN/src-train.txt -train_tgt Data/SOV_AN/tgt-train.txt -valid_src Data/SOV_AN/src-val.txt -valid_tgt Data/SOV_AN/tgt-val.txt -save_data Data/SOV_AN/prepro
+# onmt_preprocess -train_src Data/SOV_AN/src-train.txt -train_tgt Data/SOV_AN/tgt-train.txt -valid_src Data/SOV_AN/src-val.txt -valid_tgt Data/SOV_AN/tgt-val.txt -save_data Data/SOV_AN/prepro
 
-onmt_preprocess -train_src Data/SOV_NA/src-train.txt -train_tgt Data/SOV_NA/tgt-train.txt -valid_src Data/SOV_NA/src-val.txt -valid_tgt Data/SOV_NA/tgt-val.txt -save_data Data/SOV_NA/prepro
+# onmt_preprocess -train_src Data/SOV_NA/src-train.txt -train_tgt Data/SOV_NA/tgt-train.txt -valid_src Data/SOV_NA/src-val.txt -valid_tgt Data/SOV_NA/tgt-val.txt -save_data Data/SOV_NA/prepro
 
+
+onmt_train -data Data/SVO_AN -save_model Results/SVO_AN_model
+
+# onmt_train -data Data/SVO_AN -early_stopping 3 -valid_steps 1000 -save_model Results/SVO_AN_model
+
+# onmt_train -data Data/SVO_NA --early_stopping 3 -valid_steps 1000 -save_model Results/SVO_NA_model
+
+# onmt_train -data Data/SOV_AN --early_stopping 3 -valid_steps 1000 -save_model Results/SOV_AN_model
+
+# onmt_train -data Data/SOV_NA --early_stopping 3 -valid_steps 1000 -save_model Results/SOV_NA_model
