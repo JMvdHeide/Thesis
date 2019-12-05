fd9e5bb8fed2bcb6908e61db620156570211aebd
Jarik van der Heide <jmvdheide96@gmail.com>
2019-12-05 17:58:56 +0100

    Changed generate_data.py and added new data
    generate_data.py now makes sure that the target sentences in the test sets are the same over all data sets.

--- Data/generate_data.py
+++ Data/generate_data.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 from os import listdir
-from random import shuffle
+from random import shuffle, sample
 
 def clean_line(line):
 
-	line = line.strip('[\[\]]').replace(',', ' ').capitalize() #strip brackets, replace commas with spaces and capitalize first letter
-	line = '{} .'.format(line) #period et end of line
+	line = line.strip('[\[\]]').replace(',', ' ').capitalize()
+	line = '{} .'.format(line)
 
 	return line
 
-def generate_data(language_pairs, filename):
+def generate_data(language_pairs, filename, keys, split):
 
 	dirs = {"data_svo_an.txt": "SVO_AN",
 			"data_svo_na.txt": "SVO_NA",
@@ -19,30 +19,9 @@ def generate_data(language_pairs, filename):
 	}
 	directory = dirs[filename]
 
-	split = round(len(language_pairs)/10)
-
-	#Generate training data
-	train_data = language_pairs[(2*split):]
-	src_train = open("{}/src-train.txt".format(directory), "a+")
-	tgt_train = open("{}/tgt-train.txt".format(directory), "a+")
-	for pair in train_data:
-		src_train.write(pair[0] + "\n")
-		tgt_train.write(pair[1] + "\n")
-	src_train.close()
-	tgt_train.close()
-
-	#Generate validation data
-	val_data = language_pairs[split:(2*split)]
-	src_val = open("{}/src-val.txt".format(directory), "a+")
-	tgt_val = open("{}/tgt-val.txt".format(directory), "a+")
-	for pair in val_data:
-		src_val.write(pair[0] + "\n")
-		tgt_val.write(pair[1] + "\n")
-	src_val.close()
-	tgt_val.close()
-
 	#Generate test data
-	test_data = language_pairs[:split]
+	print("Generating test data...")
+	test_data = [language_pairs.pop(key) for key in keys]
 	src_test = open("{}/src-test.txt".format(directory), "a+")
 	tgt_test = open("{}/tgt-test.txt".format(directory), "a+")
 	for pair in test_data:
@@ -51,24 +30,58 @@ def generate_data(language_pairs, filename):
 	src_test.close()
 	tgt_test.close()
 
+	#Convert remaining dict values to list and shuffle
+	language_pairs = list(language_pairs.values())
+	shuffle(language_pairs)
+
+	#Generate validation data
+	print("Generating validation data...")
+	val_data = language_pairs[:split]
+	src_val = open("{}/src-val.txt".format(directory), "a+")
+	tgt_val = open("{}/tgt-val.txt".format(directory), "a+")
+	for pair in val_data:
+		src_val.write(pair[0] + "\n")
+		tgt_val.write(pair[1] + "\n")
+	src_val.close()
+	tgt_val.close()
+
+	#Generate training data
+	print("Generating training data...")
+	train_data = language_pairs[split:]
+	src_train = open("{}/src-train.txt".format(directory), "a+")
+	tgt_train = open("{}/tgt-train.txt".format(directory), "a+")
+	for pair in train_data:
+		src_train.write(pair[0] + "\n")
+		tgt_train.write(pair[1] + "\n")
+	src_train.close()
+	tgt_train.close()
+
 def main():
 	
 	path = "SCFG/Compiled"
 
+	check = 0
 	for filename in listdir(path):
+		print("Current file: {}".format(filename))
 		file_path = "{}/{}".format(path, filename)
 		with open(file_path) as file:
-			language_pairs = []
+			language_pairs = dict()
+			key = 0
 			for line in file:
 				line = line.rstrip().split(" ")
 				source = clean_line(line[0])
 				target = clean_line(line[1])
 				language_pair = (source, target)
-				language_pairs.append(language_pair)
-		# print(language_pairs)		
-		shuffle(language_pairs)
-		generate_data(language_pairs, filename)				
+				language_pairs[key] = language_pair #Put pairs in a numbered dict
+				key += 1
 
+		if check == 0: #Only generates the keys for test data once
+			split = round(len(language_pairs)/10)
+			keys = sample(range(0,len(language_pairs)), split)
+			check += 1
 
+		generate_data(language_pairs, filename, keys, split)
+		print("\n")
+		
 if __name__ == '__main__':
 	main()
