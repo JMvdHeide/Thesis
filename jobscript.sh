34a93bf272d9bd9b6d97e7b6811fdcc08a38b3ce
J.M.van.der.Heide@student.rug.nl <s2964007@peregrine.hpc.rug.nl>
2019-12-09 11:43:05 +0100

    jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -38,5 +38,4 @@ onmt_train -data Data/SVO_AN/prepro -world_size 1 -gpu_ranks 0 -train_steps 2014
 # onmt_train -data Data/SOV_NA/prepro --early_stopping 3 -valid_steps 1000 -save_model Results/SOV_NA_model
 
 
-
-# GPU: -world_size 1 -gpu_ranks 0 1
+onmt_translate -model Models/SVO_AN -src Data/SVO_AN/tgt-test.txt -output pred.txt -replace_unk -verbose
