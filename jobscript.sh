b1cd1bc966698b18c9947494b4c76efdefa1e30e
Jarik van der Heide <jmvdheide96@gmail.com>
2019-12-09 11:59:54 +0100

    Jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -3,12 +3,12 @@
 #SBATCH --time=20:00
 #SBATCH --ntasks=1
 #SBATCH --mem=20000
-#SBATCH --output=job-%j.log
+#SBATCH --output=Logs/job-%j.log
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
 
-module load Python
 source VirtualEnv/bin/activate
+module load Python
 pip install --upgrade pip
 pip install torch
 pip install torchvision
@@ -38,4 +38,4 @@ onmt_train -data Data/SVO_AN/prepro -world_size 1 -gpu_ranks 0 -train_steps 2014
 # onmt_train -data Data/SOV_NA/prepro --early_stopping 3 -valid_steps 1000 -save_model Results/SOV_NA_model
 
 
-onmt_translate -model Models/SVO_AN -src Data/SVO_AN/tgt-test.txt -output pred.txt -replace_unk -verbose
+onmt_translate -model Models/SVO_AN -src Data/SVO_AN/tgt-test.txt -output Results/SVO_AN_pred.txt -replace_unk -verbose
