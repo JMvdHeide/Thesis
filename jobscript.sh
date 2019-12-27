16737693f98a86dfb88efed4ea766ed8a93345b3
J.M.van.der.Heide@student.rug.nl <s2964007@peregrine.hpc.rug.nl>
2019-12-27 10:23:07 +0100

    Final results

--- jobscript.sh
+++ jobscript.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=onmt_test
+#SBATCH --job-name=OpenNMT
 #SBATCH --time=1:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=10000
+#SBATCH --mem=4000
 #SBATCH --output=Logs/Peregrine/job-%j.log
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
+#SBATCH --mail-user=jmvdheide96@gmail.com
+#SBATCH --mail-type=BEGIN,END
 
 module load Python
 source VirtualEnv/bin/activate
@@ -16,8 +18,16 @@ source VirtualEnv/bin/activate
 # pip install configargparse
 # pip install OpenNMT-py
 
+rm -f Models/*
+rm -f Results/*
 
 export CUDA_VISIBLE_DEVICES=0
+
+# sbatch Jobscripts/SVO_AN.sh
+# sbatch Jobscripts/SVO_NA.sh
+# sbatch Jobscripts/SOV_AN.sh
+# sbatch Jobscripts/SOV_NA.sh
+
 
 onmt_preprocess -train_src Data/SVO_AN/src-train.txt -train_tgt Data/SVO_AN/tgt-train.txt -valid_src Data/SVO_AN/src-val.txt -valid_tgt Data/SVO_AN/tgt-val.txt -save_data Data/SVO_AN/prepro
 
@@ -29,20 +39,20 @@ onmt_preprocess -train_src Data/SOV_NA/src-train.txt -train_tgt Data/SOV_NA/tgt-
 
 #--------------------------------------------------------------------------------------------------
 
-onmt_train -data Data/SVO_AN/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SVO_AN_model -batch_type sents -log_file Logs/OpenNMT/SVO_AN.log -early_stopping 3 -train_steps 2014 -valid_steps 50
+onmt_train -data Data/SVO_AN/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SVO_AN_model -log_file Logs/OpenNMT/SVO_AN.log -early_stopping 3 -train_steps 1000 -valid_steps 50
 
-onmt_train -data Data/SVO_NA/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SVO_NA_model -batch_type sents -log_file Logs/OpenNMT/SVO_NA.log -early_stopping 3 -train_steps 2014 -valid_steps 50
+onmt_train -data Data/SVO_NA/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SVO_NA_model -log_file Logs/OpenNMT/SVO_NA.log -early_stopping 3 -train_steps 1000 -valid_steps 50
 
-onmt_train -data Data/SOV_AN/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SOV_AN_model -batch_type sents -log_file Logs/OpenNMT/SOV_AN.log -early_stopping 3 -train_steps 2014 -valid_steps 50
+onmt_train -data Data/SOV_AN/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SOV_AN_model -log_file Logs/OpenNMT/SOV_AN.log -early_stopping 3 -train_steps 1000 -valid_steps 50
 
-onmt_train -data Data/SOV_NA/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SOV_NA_model -batch_type sents -log_file Logs/OpenNMT/SOV_NA.log -early_stopping 3 -train_steps 2014 -valid_steps 50
+onmt_train -data Data/SOV_NA/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SOV_NA_model -log_file Logs/OpenNMT/SOV_NA.log -early_stopping 3 -train_steps 1000 -valid_steps 50
 
 #--------------------------------------------------------------------------------------------------
 
-onmt_translate -model Models/SVO_AN_model*.pt -src Data/SVO_AN/src-val.txt -output Results/SVO_AN_pred.txt -batch_type sents -replace_unk -verbose
+onmt_translate -model Models/SVO_AN_model*.pt -src Data/SVO_AN/src-test.txt -output Results/SVO_AN_pred.txt -replace_unk -verbose
 
-onmt_translate -model Models/SVO_NA_model*.pt -src Data/SVO_NA/src-val.txt -output Results/SVO_NA_pred.txt -batch_type sents -replace_unk -verbose
+onmt_translate -model Models/SVO_NA_model*.pt -src Data/SVO_NA/src-test.txt -output Results/SVO_NA_pred.txt -replace_unk -verbose
 
-onmt_translate -model Models/SOV_AN_model*.pt -src Data/SOV_AN/src-val.txt -output Results/SOV_AN_pred.txt -batch_type sents -replace_unk -verbose
+onmt_translate -model Models/SOV_AN_model*.pt -src Data/SOV_AN/src-test.txt -output Results/SOV_AN_pred.txt -replace_unk -verbose
 
-onmt_translate -model Models/SOV_NA_model*.pt -src Data/SOV_NA/src-val.txt -output Results/SOV_NA_pred.txt -batch_type sents -replace_unk -verbose
+onmt_translate -model Models/SOV_NA_model*.pt -src Data/SOV_NA/src-test.txt -output Results/SOV_NA_pred.txt -replace_unk -verbose
