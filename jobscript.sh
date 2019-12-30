3187f31f6048f4fd07a5c52ed02aac6289429d0e
J.M.van.der.Heide@student.rug.nl <s2964007@peregrine.hpc.rug.nl>
2019-12-30 09:35:43 +0100

    After some mistakes made new repo and added .gitignore

--- jobscript.sh
+++ jobscript.sh
@@ -7,19 +7,25 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
 #SBATCH --mail-user=jmvdheide96@gmail.com
-#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-type=BEGIN,END,FAIL
 
 module load Python
 source VirtualEnv/bin/activate
-# pip install --upgrade pip
-# pip install torch
-# pip install torchvision
-# pip install torchtext
-# pip install configargparse
-# pip install OpenNMT-py
+pip install --upgrade pip
+pip install torch
+pip install torchvision
+pip install torchtext
+pip install configargparse
+pip install OpenNMT-py
 
-rm -f Models/*
-rm -f Results/*
+for struct in SVO_AN SVO_NA SOV_AN SOV_NA
+do
+	rm -f Models/${struct}/*.pt
+	rm -f Results/${struct}*.txt
+	rm -f Data/${struct}/*.pt
+done
+
+rm -f Logs/OpenNMT/*.log
 
 export CUDA_VISIBLE_DEVICES=0
 
@@ -39,20 +45,31 @@ onmt_preprocess -train_src Data/SOV_NA/src-train.txt -train_tgt Data/SOV_NA/tgt-
 
 #--------------------------------------------------------------------------------------------------
 
-onmt_train -data Data/SVO_AN/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SVO_AN_model -log_file Logs/OpenNMT/SVO_AN.log -early_stopping 3 -train_steps 1000 -valid_steps 50
+onmt_train -data Data/SVO_AN/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SVO_AN/SVO_AN_model -log_file Logs/OpenNMT/SVO_AN.log -train_steps 450 -valid_steps 25 -report_every 25 -save_checkpoint_steps 25
 
-onmt_train -data Data/SVO_NA/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SVO_NA_model -log_file Logs/OpenNMT/SVO_NA.log -early_stopping 3 -train_steps 1000 -valid_steps 50
+onmt_train -data Data/SVO_NA/prepro -world_size 1 -gpu_ranks 0 -save_model Models//SVO_NA/SVO_NA_model -log_file Logs/OpenNMT/SVO_NA.log -train_steps 450 -valid_steps 25 -report_every 25 -save_checkpoint_steps 25
 
-onmt_train -data Data/SOV_AN/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SOV_AN_model -log_file Logs/OpenNMT/SOV_AN.log -early_stopping 3 -train_steps 1000 -valid_steps 50
+onmt_train -data Data/SOV_AN/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SOV_AN/SOV_AN_model -log_file Logs/OpenNMT/SOV_AN.log -train_steps 450 -valid_steps 25 -report_every 25 -save_checkpoint_steps 25
 
-onmt_train -data Data/SOV_NA/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SOV_NA_model -log_file Logs/OpenNMT/SOV_NA.log -early_stopping 3 -train_steps 1000 -valid_steps 50
+onmt_train -data Data/SOV_NA/prepro -world_size 1 -gpu_ranks 0 -save_model Models/SOV_NA/SOV_NA_model -log_file Logs/OpenNMT/SOV_NA.log -train_steps 450 -valid_steps 25 -report_every 25 -save_checkpoint_steps 25
 
 #--------------------------------------------------------------------------------------------------
 
-onmt_translate -model Models/SVO_AN_model*.pt -src Data/SVO_AN/src-test.txt -output Results/SVO_AN_pred.txt -replace_unk -verbose
+for struct in SVO_AN SVO_NA SOV_AN SOV_NA
+do
 
-onmt_translate -model Models/SVO_NA_model*.pt -src Data/SVO_NA/src-test.txt -output Results/SVO_NA_pred.txt -replace_unk -verbose
+	for i in {25..450..25}
+	do
+		model=Models/${struct}/${struct}_model_step_${i}.pt
+		echo $model
+		onmt_translate -model $model -src Data/${struct}/src-val.txt -output Results/${struct}/${struct}_step_${i}_pred.txt -replace_unk
+	done
 
-onmt_translate -model Models/SOV_AN_model*.pt -src Data/SOV_AN/src-test.txt -output Results/SOV_AN_pred.txt -replace_unk -verbose
 
-onmt_translate -model Models/SOV_NA_model*.pt -src Data/SOV_NA/src-test.txt -output Results/SOV_NA_pred.txt -replace_unk -verbose
+# onmt_translate -model Models/SVO_NA_model*.pt -src Data/SVO_NA/src-val.txt -output Results/SVO_NA_pred.txt -replace_unk -verbose
+
+# onmt_translate -model Models/SOV_AN_model*.pt -src Data/SOV_AN/src-val.txt -output Results/SOV_AN_pred.txt -replace_unk -verbose
+
+# onmt_translate -model Models/SOV_NA_model*.pt -src Data/SOV_NA/src-val.txt -output Results/SOV_NA_pred.txt -replace_unk -verbose
+
+done
